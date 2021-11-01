88652e20abeab63952de50f7a9bc946846583e12
Felipe Marinho <felipegigamail@gmail.com>
2021-10-31 21:06:27 -0300

    Update connect

--- links.js
+++ links.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   var links = [
     {
       label: "Connect",
-      href: "https://login.microsoftonline.com/b7be7686-6f97-4db7-9081-a23cf09a96b5/saml2?SAMLRequest=rZNNj5swEIb%2FCvKd7wDBCqnSRFUj9QPtRnvopRqMSSwZm3pMN%2F33NSS7m0Ob%0AQ9WTJc87M%2B8zY68QejnQzWhP6oH%2FGDla79xLhXQOVGQ0impAgVRBz5FaRh83%0Anz%2FRJIjoYLTVTEvi7VyeUGCFVhU5WTsgDUOpj0IFvWBGo%2B6sVlIoHjDdh03R%0A8CJf5n7elYW%2FaJvCL6Nl7EOSsi4qocybLJwMJMT7oA3js7%2BKRMTb7yryfZGk%0A2aJL4wIiaNo0TViesxhSvsyWRbxwWXvEke8VWlC2IkkUl36c%2BFF%2BiFOaLWlW%0AfiNefbX%2FXqhWqON91uYiQvrxcKj9%2BuvjYS7wU7TcfHHqN2xgjCPaDoITSCma%0A0Tj0GbsFBb6CEBzNjOdz1Q5aKBuwo3g3VDjExHviBuc5urZkvZp0dMYxN5u5%0AbxYQuZmWQdb%2Fx9UqvLFx8TTQCXu%2Fq7UU7Je3kVI%2Fbw0H60YRk%2FBFdH1UvJ1X%0AuNXK8rP1trofwAicMPkZmH0BvVVtpeN44N2%2FYN%2BVMcqm0u66dsezNu2V70%2Fd%0A15fYX0heo7c%2FaP0b%0A&amp;RelayState=https%3A%2F%2Faccesstfa.halliburton.com%2Fmfa",
+      href: "https://accesstfa.halliburton.com/mfa/",
       description: "Página inicial"
     },
     {
