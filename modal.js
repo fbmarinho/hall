90c611eff6a89fdcf8d3aefc23866743708f22dd
Felipe Marinho <felipegigamail@gmail.com>
2023-09-10 09:23:55 -0300

    Add modal

---
+++ modal.js
@@ -0,0 +1,48 @@
+
+function openDialog(){
+    document.querySelector('dialog').showModal();
+}
+function closeDialog(){
+    document.querySelector('dialog').close();
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+    createModal("VPN Login","http://accesstfa.halliburton.com/mfa/");
+   
+    lucide.createIcons();
+})
+
+
+function createModal(title, url){
+
+    const modal = document.createElement('dialog');
+    
+    const modalTitle = document.createElement('div');
+
+
+    const span = document.createElement('div');
+    span.innerText = title;
+    
+    
+    const icon = document.createElement('i');
+    icon.dataset.lucide = 'x';
+    icon.innerText = "X"
+
+    const closebtn = document.createElement('button');
+    closebtn.addEventListener('click',closeDialog);
+    closebtn.append(icon);
+    
+
+    const iframe = document.createElement('iframe');
+    iframe.src = url;
+
+    modalTitle.append(span);
+    modalTitle.append(closebtn);
+
+    modal.append(modalTitle);
+    modal.append(iframe);
+
+    document.body.append(modal);
+
+    modal.close();
+}
