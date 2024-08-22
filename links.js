ca3d4fab92ef991bf8e47e018cef63b4680a8f67
Felipe Marinho <felipegigamail@gmail.com>
2024-08-22 08:44:29 -0300

    Update links.js

--- links.js
+++ links.js
@@ -435,8 +435,10 @@ document.addEventListener("DOMContentLoaded", function() {
       
       item.appendChild(linkel);
       //get div and append
+      if(  link.type != 'vpn' ){
       let divisoria = document.getElementById(link.type || "direto");
-      link.type != 'vpn' && divisoria.appendChild(item);
+      divisoria.appendChild(item);
+      }
     }
   }
   lucide.createIcons();
