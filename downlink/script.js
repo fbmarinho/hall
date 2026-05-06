631685a2c1f8c1a7809286d0cf4db92bf870d219
Felipe Marinho <felipegigamail@gmail.com>
2026-05-06 17:37:05 -0300

    Update Downlink advisor

--- downlink/script.js
+++ downlink/script.js
@@ -13,22 +13,26 @@ window.addEventListener("DOMContentLoaded", () => {
   const vpasdInput = document.getElementById("vpasd");
   const noiseInput = document.getElementById("noise");
 
-  threshold = parseFloat(thresholdInput.value) || 0;
-  vpa = parseFloat(vpaInput.value) || 0;
-  vpasd = parseFloat(vpasdInput.value) || 0;
-  noise = parseFloat(noiseInput.value) || 0;
+  threshold = parseFloat(window.localStorage.getItem("threshold")) || 250;
+  vpa = parseFloat(window.localStorage.getItem("vpa")) || 800;
+  vpasd = parseFloat(window.localStorage.getItem("vpasd")) || 50;
+  noise = parseFloat(window.localStorage.getItem("noise")) || 50;
 
   thresholdInput.addEventListener("input", () => {
     threshold = parseFloat(thresholdInput.value) || 0;
+    window.localStorage.setItem("threshold", threshold);
   });
   vpaInput.addEventListener("input", () => {
     vpa = parseFloat(vpaInput.value) || 0;
+    window.localStorage.setItem("vpa", vpa);
   });
   vpasdInput.addEventListener("input", () => {
     vpasd = parseFloat(vpasdInput.value) || 0;
+    window.localStorage.setItem("vpasd", vpasd);
   });
   noiseInput.addEventListener("input", () => {
     noise = parseFloat(noiseInput.value) || 0;
+    window.localStorage.setItem("noise", noise);
   });
 
   canvas = document.getElementById("canvas");
@@ -45,7 +49,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   function update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+    drawNoisePulses();
     drawNoise();
     drawThresholdLine();
     drawVPAline();
@@ -57,12 +61,17 @@ window.addEventListener("DOMContentLoaded", () => {
 
 function drawThresholdLine() {
   const y = canvas.height - (threshold / 2000) * canvas.height;
+  ctx.setLineDash([15, 5]);
   ctx.strokeStyle = "#FFF";
-  ctx.lineWidth = 8;
+  ctx.lineWidth = 4;
   ctx.beginPath();
   ctx.moveTo(0, y);
-  ctx.lineTo(canvas.width, y);
+  ctx.lineTo(canvas.width - 120, y);
   ctx.stroke();
+  ctx.font = "16px serif";
+  ctx.fillStyle = "#ffffff";
+  ctx.textAlign = "left";
+  ctx.fillText(`Threshold: ${threshold}`, canvas.width - 110, y + 3);
 }
 
 function drawPulses() {
@@ -96,7 +105,36 @@ function drawVPAline() {
   ctx.lineWidth = 2;
   ctx.beginPath();
   ctx.moveTo(xpos, y);
-  ctx.lineTo(canvas.width, y);
+  ctx.lineTo(canvas.width - 110, y);
+
+  ctx.stroke();
+
+  ctx.font = "14px serif";
+  ctx.fillStyle = "#ffffff";
+  ctx.textAlign = "left";
+  ctx.fillText(`VPA Mean: ${vpa}`, canvas.width - 100, y + 3);
+}
+
+function drawNoisePulses() {
+  const desvio = canvas.height - 3 * (noise / 2000) * canvas.height;
+  ctx.strokeStyle = "#651111";
+  ctx.beginPath();
+  ctx.setLineDash([0, 0]);
+  ctx.moveTo(0, canvas.height);
+
+  for (let i = 0; i <= 198; i++) {
+    ctx.lineTo(
+      canvas.width / 200 + i * (canvas.width / 200),
+      i % 2 === 0 ? desvio : canvas.height - 4,
+    );
+    // ctx.arc(
+    //   canvas.width / 100 + i * (canvas.width / 100),
+    //   i % 2 === 0 ? desvio : canvas.height - 4,
+    //   4,
+    //   0,
+    //   2 * Math.PI,
+    // );
+  }
 
   ctx.stroke();
 }
@@ -105,11 +143,11 @@ function drawNoise() {
   const y = canvas.height - (noise / 2000) * canvas.height;
   const ysigma = canvas.height - ((3 * noise) / 2000) * canvas.height;
 
-  ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
-  ctx.beginPath();
-  ctx.rect(0, ysigma, canvas.width, ysigma);
-  ctx.fill();
-
+  // ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
+  // ctx.beginPath();
+  // ctx.rect(0, ysigma, canvas.width, ysigma);
+  // ctx.fill();
+  ctx.setLineDash([5, 5]);
   ctx.fillStyle = "rgba(0, 166, 30, 0.76)";
   ctx.beginPath();
   ctx.rect(
@@ -124,11 +162,13 @@ function drawNoise() {
   ctx.lineWidth = 2;
   ctx.beginPath();
   ctx.moveTo(0, y);
-  ctx.lineTo(canvas.width, y);
+  ctx.lineTo(canvas.width - 120, y);
   ctx.stroke();
 
-  ctx.font = "12px serif";
+  ctx.font = "16px serif";
   ctx.fillStyle = "#ffffff";
-  ctx.fillText(`Noise SD: ${noise}`, 10, y - 5);
-  ctx.fillText(`Noise 3Sigma: ${3 * noise}`, 10, ysigma - 5);
+  ctx.textAlign = "left";
+  ctx.fillText(`Noise SD: ${noise}`, canvas.width - 110, y + 3);
+  ctx.textAlign = "center";
+  ctx.fillText(`Noise 3Sigma: ${3 * noise}`, canvas.width / 2, ysigma + 20);
 }
